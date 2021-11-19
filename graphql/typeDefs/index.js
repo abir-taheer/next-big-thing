e37554b89155b967738556e68b94bf6e36810c13
Abir Taheer <abir@taheer.me>
2021-11-19 04:47:33 -0500

    added scalars and changed id type to object id

--- graphql/typeDefs/index.js
+++ graphql/typeDefs/index.js
@@ -1,6 +1,7 @@
 import Query from "./Query";
 import User from "./User";
+import { typeDefs as scalarTypeDefs } from "graphql-scalars";
 
-const typeDefs = [User, Query];
+const typeDefs = [User, Query, scalarTypeDefs];
 
 export default typeDefs;
